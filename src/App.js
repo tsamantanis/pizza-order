168695cdc4ca45ea1b4218d1b1a261eefb7e856a
tsamantanis <philip.tsamantanis@icloud.com>
2021-09-13 12:33:32 -0700

    Pizza order lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('')
+  const [peperoni, setPeperoni] = useState(false)
+  const [sausage, setSausage] = useState(false)
+  const [peppers, setPeppers] = useState(false)
+  const [onions, setOnions] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <h1>Pizza Order</h1>
+      <form>
+
+        <div className="col">
+          <input type="text" className="name"  onChange={(e) => setName(e.target.value)} placeholder="Name" />
+          <div className="checkboxes">
+            <label>
+              <input type="checkbox" value={peperoni} onChange={() => setPeperoni(!peperoni)} />
+              Peperoni
+            </label>
+            <label>
+              <input type="checkbox" value={sausage} onChange={() => setSausage(!sausage)} />
+              Sausage
+            </label>
+            <label>
+              <input type="checkbox" value={peppers} onChange={() => setPeppers(!peppers)} />
+              Peppers
+            </label>
+            <label>
+              <input type="checkbox" value={onions} onChange={() => setOnions(!onions)} />
+              Onions
+            </label>
+          </div>
+        </div>
+        <div className="col">
+          <h3>Your Order</h3>
+          {name && <div className="margin-bottom">{name}</div>}
+          <div className="ingredients">
+           {peperoni && <div>Peperoni</div>}
+           {sausage && <div>Sausage</div>}
+           {peppers && <div>Peppers</div>}
+           {onions && <div>Onions</div>}
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
